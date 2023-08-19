a342fbaf9ec366e9dc67a124430e90e5478e6b82
shivamgiri007 <shivamgiri.ng@gmail.com>
2023-08-19 11:48:27 +0530

    Dark Mode set

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,5 @@
 import React , {useState} from "react";
-
+import "./Main.css";
 
 export default function Main(props) {
 const [text , setText] = useState("Enter text here..");
@@ -27,24 +27,41 @@ const clearText = () => {
 const handleOnChange = (event) => {   
     setText(event.target.value);
 }
-    
+function countLettersWithoutSpaces(newtext) {
+  let numLetters = 0;
+  for (let i = 0; i < newtext.length; i++) {
+    if (newtext[i] !== " ") {
+      numLetters++;
+    }
+  }
+  return numLetters;
+}
+function countLettersSpaces(newtext) {
+  let spaceLetters = 0;
+  for (let i = 0; i < newtext.length; i++) {
+    if (newtext[i] === " ") {
+      spaceLetters++;
+    }
+  }
+  return spaceLetters;
+}   
     return (
-    <div style={props.theme}>
-      <div>
+      <>
+    <div className="main_height" style={props.theme}>
         <div className="w-50 mx-auto">
-        <h3>Enter text for operations</h3>
+        <h3 className="p-4">Enter text for operations</h3>
 
           <textarea
           value={text}
           onChange={handleOnChange}
-            style={props.theme}
-            class="form-control"
-            id="exampleFormControlTextarea1"
-            rows="8"
+          style={props.theme}
+          class="form-control"
+          id="exampleFormControlTextarea1"
+          rows="8"
           ></textarea>
           
         </div>
-      </div>
+      
       <div className="d-flex justify-content-center">
       <button className="btn btn-primary mt-4 mx-1" onClick={toUpperCase}>toUpperCase</button>
           <button className="btn btn-primary mt-4 mx-1" onClick={toLowerCase}>toLowerCase</button>
@@ -53,6 +70,13 @@ const handleOnChange = (event) => {
           <button className="btn btn-primary mt-4 mx-1" onClick={copyText}>Copy Text</button>
           <button className="btn btn-primary mt-4 mx-1" onClick={clearText}>Clear Text</button>
       </div>
+      <div className="w-50 mx-auto p-5">
+        <h4 className="text-decoration-underline">Text Summary</h4>
+        <h6>Total Text Count without spaces is  : {countLettersWithoutSpaces(newtext)} </h6> 
+        <h6>Total spaces is text is  : {countLettersSpaces(newtext)} </h6> 
+      </div>
+      
     </div>
+    </>  
   );
 }
